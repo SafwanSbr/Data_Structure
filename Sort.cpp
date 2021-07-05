f2a1001ccff1f70d4b9b745ed7bf898e6f0609a1
SafwanSbr <84856781+SafwanSbr@users.noreply.github.com>
2021-07-05 22:17:21 +0600

    Update Sort.cpp

--- Sort.cpp
+++ Sort.cpp
@@ -30,19 +30,21 @@ void SelectionSort(int arr[], int Size){
 }
 //Insertion Sort
 void InsertionSort(int arr[], int Size){
-    for(int i=0; i<Size; i++){
+    for(int i=1; i<Size; i++){
         int v=arr[i];
-        for(int j=i; j>=0; j--){
+        for(int j=i-1; j>=0; j--){
+            //cout<<"I="<<arr[i]<<" J="<<arr[j]<<" .................................\n";
             if(arr[i]<arr[j]) Swap(&arr[i--], &arr[j]);
-            // cout<<"I="<<arr[i]<<" J="<<arr[j]<<" ";
-            // cout<<"\n";
+            else break;
+             //cout<<"I="<<arr[i]<<" J="<<arr[j]<<" ";
+             //cout<<"\n";
         } //for(int i=0; i<Size; i++) cout<<arr[i]<<" "; cout<<"\n";
     }
     for(int i=0; i<Size; i++) cout<<arr[i]<<" ";
 }
 
 int main(){
-    int arr[5]={5, 2, 3, 1, 4};
+    int arr[]={7,3,9,1,0,33,6};
     int Size=sizeof(arr)/sizeof(arr[0]);
     BubbleSort(arr, Size);    cout<<"\n";
     SelectionSort(arr, Size);   cout<<"\n";
