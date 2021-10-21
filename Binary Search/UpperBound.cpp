8a0e0328e5b25606dcd803ebf8f137c7cafc0ac1
SafwanSbr <84856781+SafwanSbr@users.noreply.github.com>
2021-10-21 23:48:43 +0600

    Create UpperBound.cpp

---
+++ Binary Search/UpperBound.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int UpperBou_Binary_search( int arr[], int Size, int element)
+{
+    int l=0, r=Size-1;
+    int ans = Size;
+    
+    while( l<=r )
+    {
+        int mid = (l+r) / 2;
+        
+        if( arr[mid] <= element ){
+            ans = min( ans, mid);
+            r = mid - 1;
+        }
+        else l=mid+1;
+    }
+    
+    return ans;
+}
+
+int main()
+{
+}
